23bb44c006474c05b8b26517713532e3d1ff63e3
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-04 10:46:17 +0100

    feature: added indentation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -23,7 +23,7 @@ export const decorators = [
  */
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+   actions: { argTypesRegex: '^on[A-Z].*' },
    // Storybook a11y addon configuration
    a11y: {
      // the target DOM element
